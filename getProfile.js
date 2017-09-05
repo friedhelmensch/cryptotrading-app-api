210b2f3c86e0eeb0a96f26605c66870c4179c62e
ffnoname <ffnoname@gmx.de>
2017-09-05 20:53:18 +0200

    get profile return null if not found

--- getProfile.js
+++ getProfile.js
@@ -12,13 +12,7 @@ export async function main(event, context, callback) {
 
   try {
     const result = await dynamoDbLib.call('query', params);
-    if (result.Items[0]) {
-      // Return the retrieved item
-      callback(null, success(result.Items[0]));
-    }
-    else {
-      callback(null, failure({status: false, error: 'Profile not found.'}));
-    }
+    callback(null, success(result.Items[0]));
   }
   catch(e) {
     callback(null, failure({status: false}));
