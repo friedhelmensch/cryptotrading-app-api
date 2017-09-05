653f08839e274ee90783d42a5c23cf20846e9b45
ffnoname <ffnoname@gmx.de>
2017-09-05 20:39:41 +0200

    query for get and delete profile

--- getProfile.js
+++ getProfile.js
@@ -4,19 +4,20 @@ import { success, failure } from './libs/response-lib';
 export async function main(event, context, callback) {
   const params = {
     TableName: 'profiles',
-    Key: {
-      userId: event.requestContext.identity.cognitoIdentityId
-    },
+    KeyConditionExpression: "userId = :userId",
+    ExpressionAttributeValues: {
+      ":userId": event.requestContext.identity.cognitoIdentityId,
+    }
   };
 
   try {
-    const result = await dynamoDbLib.call('get', params);
-    if (result.Item) {
+    const result = await dynamoDbLib.call('query', params);
+    if (result.Items[0]) {
       // Return the retrieved item
-      callback(null, success(result.Item));
+      callback(null, success(result.Items[0]));
     }
     else {
-      callback(null, failure({status: false, error: 'Item not found.'}));
+      callback(null, failure({status: false, error: 'Profile not found.'}));
     }
   }
   catch(e) {
