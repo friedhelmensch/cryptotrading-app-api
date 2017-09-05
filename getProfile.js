aaa0adfe0b0aa51232a1cd8633b94c066aa9c8cb
ffnoname <ffnoname@gmx.de>
2017-09-05 21:38:06 +0200

    return empty json instead of undefined

--- getProfile.js
+++ getProfile.js
@@ -12,7 +12,10 @@ export async function main(event, context, callback) {
 
   try {
     const result = await dynamoDbLib.call('query', params);
+    if(result.Items[0])
     callback(null, success(result.Items[0]));
+    else
+      callback(null, success({}));
   }
   catch(e) {
     callback(null, failure({status: false}));
