848363ab5cfc523899fe0a822096109c35c0191d
ffnoname <ffnoname@gmx.de>
2017-09-21 22:48:29 +0200

    mask api key and secret on get profile

--- getProfile.js
+++ getProfile.js
@@ -12,12 +12,32 @@ export async function main(event, context, callback) {
 
   try {
     const result = await dynamoDbLib.call('query', params);
-    if(result.Items[0])
-    callback(null, success(result.Items[0]));
-    else
+    const profile = result.Items[0];
+    if (profile) {
+      
+      var apiKey = null;
+      var apiSecret = null;
+      if (profile.apiKey && profile.apiSecret) {
+        apiKey = profile.apiKey.substring(0, 5) + "*********************";
+        apiSecret = profile.apiSecret.substring(0, 5) + "*********************"
+      }
+      else {
+        apiKey = "invalid";
+        apiSecret = "invalid";
+      }
+
+      var returnValue = {
+        apiKey: apiKey,
+        apiSecret: apiSecret,
+        active: profile.active
+      }
+      callback(null, success(returnValue));
+    } else {
       callback(null, success({}));
+    }
+
   }
-  catch(e) {
-    callback(null, failure({status: false}));
+  catch (e) {
+    callback(null, failure({ status: false }));
   }
 };
